ec0d95268e92ef123584b314477713dacd48a4d5
AhmedAGadir <ahmed.gadir@gmail.com>
2019-08-07 18:52:59 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import { GlobalStyle } from './styles';
-import { eventsArray } from './events';
-import EventCard from './Components/EventCard';
+
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import Home from './Pages/Home';
+import Events from './Pages/Events';
+import AboutUs from './Pages/AboutUs';
 
 const App = () => (
-  <>
-    <GlobalStyle />
-    <div>
-      <h1>Sudanese events</h1>
-        <ul>
-          { eventsArray.map((event, index) => <EventCard event={event} key={index} />) }
-        </ul>
-    </div>
-  </>
-  );
+	<>
+		<GlobalStyle />
+		<BrowserRouter>
+			<Route path="/" exact component={Home} />
+			<Route path="/events" exact component={Events} />
+			<Route path="/about-us" component={AboutUs} />
+
+			<NavLink
+				to='/'
+				// activeClassName={classes.active}
+				exact>Home
+    		</NavLink>
+			<NavLink
+				to='/events'
+				// activeClassName={classes.active}
+				exact>events
+    		</NavLink>
+			<NavLink
+				to='/about-us'
+				// activeClassName={classes.active}
+				exact>about us
+    		</NavLink>
+		</BrowserRouter>
+	</>
+);
 
 export default App;
---
+++ src/Pages/Events.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Events = props => (
+    <h1>Events</h1>
+);
+
+export default Events;
+
+// import { eventsArray } from './events';
+// import EventCard from './Components/EventCard';
+// <div>
+//       <h1>Sudanese events</h1>
+//         <ul>
+//           { eventsArray.map((event, index) => <EventCard event={event} key={index} />) }
+//         </ul>
+//     </div>
