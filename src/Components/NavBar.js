4201522bed16994db24ae19a013fc095da5af292
Sarah Elderderi <sarahelderderi@Sarahs-MacBook-Pro.local>
2019-08-07 19:01:54 +0100

    update

---
+++ src/Components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+      <ul>
+        <li><a href="default.asp">Home</a></li>
+        <li><a href="events.html">Events</a></li>
+        <li><a href="contact.html">Contact</a></li>
+        <li><a href="about.html">About</a></li>
+      </ul>
+    );
+  }
+
+export default Navbar;
