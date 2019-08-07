7969a3191df7aff66f0fdb913843c6fffebcbba9
AhmedAGadir <ahmed.gadir@gmail.com>
2019-08-07 20:42:06 +0100

    update

--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+// import EventsArray from '../events';
 
 const Gallery = props => {
     console.log('props', props);
+    // useEffect(() => {
+    //     if (condition) {
+    //         // if props.location.state is an empty array, then set them here dynamically or go back to the events page
+
+    //         // each event also needs an id
+    //     }
+    // }, []);
     return (
         <>
             <h1>{props.location.state.eventName}</h1>
