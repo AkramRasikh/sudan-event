eaa20c1f64c8c6e961bfce9347d3892af06199db
AhmedAGadir <ahmed.gadir@gmail.com>
2019-08-07 19:46:43 +0100

    update

--- src/App.js
+++ src/App.js
@@ -16,11 +16,7 @@ const App = () => (
 			<Route path="/" exact component={Home} />
 			<Route path="/events" exact component={Events} />
 			<Route path="/about-us" exact component={AboutUs} />
-<<<<<<< HEAD
-		</BrowserRouter>
-=======
 		</Layout>
->>>>>>> cb74554b9e060ac5cbb03ca5664eda2c74d19d3c
 	</>
 );
 
