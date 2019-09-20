fcce8d65213f59cdf5f876ff2d18c78f9257f34e
sarahelderderi <sarahelderderi@gmail.com>
2019-09-20 22:08:53 +0100

    hadeal

--- src/App.js
+++ src/App.js
@@ -3,19 +3,11 @@ import { Route } from 'react-router-dom'
 
 import { GlobalStyle } from './styles';
 
-import Layout from './Components/Layout';
-import Home from './Pages/Home';
-import Events from './Pages/Events';
-import About from './Pages/About';
-import EventGallery from './Pages/EventGallery';
-import ProjectGallery from './Pages/ProjectGallery';
-import Contact from './Pages/Contact';
-import Projects from './Pages/Projects';
 
 
 const App = () => (
 	<>
-		
+
 	</>
 );
 
